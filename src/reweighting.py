Use 10% as warmup and fix delta_u calculation.

src/reweighting.py
@@ -2,19 +2,19 @@
 import numpy as np
 import arviz as az
 
-from inference_loop import LAMBDAS
+from inference_loop import LAMBDAS, ZERO_TEMP
 
 rng = np.random.default_rng(130118)
 
 iData = az.from_netcdf('./samples.nc')
 
 x_samps = iData.posterior['x'].values
+warmup = int(round(x_samps.shape[1] * 0.1))
 
-warmup = 1000
 v_k = iData.sample_stats['bias_value'].values
 logp = iData.sample_stats['lp'].values
 lambda_idx = 0
-delta_u = -LAMBDAS[None, None, :] * logp[:, :, None]
+delta_u = -LAMBDAS[None, None, :] / ZERO_TEMP * logp[:, :, None]
 
 weights = np.exp(-delta_u[:, warmup:, ] + v_k[:, warmup:, None])
 weights = weights / np.sum(weights, axis=1)[:, None]
@@ -34,4 +34,5 @@ for it, weights_i in enumerate(weights_at):
         replace=True,
         p=weights_i,
     ).astype(int)
+
     x_resampled[it] = np.copy(x_samps[it, indices, :])